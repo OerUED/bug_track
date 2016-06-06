01b05ab7068b5603271f91b1050fcb61069c5b46
maat <maat@ixiaopu.com>
2016-06-06 15:41:36 +0800

    修改origin header

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ app.use(session({
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin',req.headers.origin || '*');
     res.setHeader('Access-Control-Allow-Credentials',true);
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,X_Requested_With,accept,DEVICE_ID,content-type');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,OPTIONS,DELETE,PUT');
   next();
 });
 
