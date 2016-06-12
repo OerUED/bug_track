834ec680f7af1674cac8108ec8da31196969f23c
maat <maat@ixiaopu.com>
2016-06-12 15:30:53 +0800

    d

--- routes/log.js
+++ routes/log.js
@@ -7,7 +7,7 @@ var controller = require('../controllers/log');
 var router = express.Router();
 //后台管理
 router.post('/add',controller.add);
-router.get('/list',auth.hasRole,controller.list);
+router.get('/list',controller.list);
 router.delete('/:id', auth.hasRole, controller.delete);
 router.delete('/appKey/:appKey',auth.hasRole, controller.deleteByAppKey);
 router.delete('/type/:type',auth.hasRole, controller.deleteByType);
