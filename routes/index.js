ef1306f0a8b179a5922c70dc140e3d29cc28c438
maat <maat@ixiaopu.com>
2016-07-15 19:41:45 +0800

    d

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,15 @@ module.exports = function(app) {
   app.use('/domain', require('./domain'));
   app.use('/log', require('./log'));
   app.use('/user', require('./user'));
-
-  app.use('/*', function (req,res,next) {
-    return res.json({code: 200, data:'咩～'});
+  app.use('/now', function(req, res, next) {
+    return res.json({
+      status: 1,
+      data: new Date().getTime(),
+      details: null,
+      msg: ""
+    });
+  })
+  app.use('/*', function(req, res, next) {
+    return res.json({ code: 200, data: '咩～' });
   })
 };
